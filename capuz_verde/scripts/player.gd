39f5a2133bd23097f60d44017d7f0eff0e93f936
Rogério Rocha <neorogerio@gmail.com>
2022-11-10 13:01:48 -0300

    Aprimoramento de script do player

--- capuz_verde/scripts/player.gd
+++ capuz_verde/scripts/player.gd
@@ -4,11 +4,10 @@ onready var changer = get_parent().get_node("transition_in")
 # variaveis fisica e movimentos
 var velocity = Vector2.ZERO
 var move_speed = 60
-var gravity =450
+var gravity = 450
 var jump_force = -400
 var move_direction : int
 var direction:int = 1
-var can_move = true
 # variaveis estados do personagem
 var anim
 var dead
@@ -19,34 +18,48 @@ var levou_dano = false
 onready var collision: CollisionShape2D = get_node("AreaDeAtack/Collision")
 onready var timer := $Timer as Timer
 
+################################
+# MAQUINA DE ESTADOS DO PLAYER #
+################################
+signal state_change
+
+var state_stack = []
+var current_state = null
+
+onready var state_map = {
+	'idle': $States/Idle,
+	'move': $States/Move,
+	'jump': $States/Jump,
+	'hit': $States/Hit
+}
+
 func _ready():
+	state_stack.push_front($States/Idle)
+	current_state = state_stack[0]
+	#_change_state('idle')
+	
 	dead = false
 	anim = "idle"
 	position.x = Global.player_position_x
 	position.y = Global.player_position_y
 
 func _physics_process(delta):
+	var state_name = current_state.update(self, delta)
+	if state_name:
+		#_change_state(state_name)
+	
+	health = Global.health
+	can_attack = true
 	
 	velocity.y += gravity * delta
-
+	velocity.x = 0
+	
 	if health > Global.health:
 		timer.start()
 		print("[player.gd] levou dano --->") #####
 		velocity.y = jump_force / 4
 		$Sprite.modulate = "ff7a7a"
-	
-	health = Global.health
-	can_move = Global.can_move
-	can_attack = true
-	
-	############ funcoes sempre rodando
-	_get_input()
-	attack()
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-	_set_animation()
-	
-func _get_input():
-	velocity.x = 0
+		
 	move_direction = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	velocity.x = move_speed * move_direction
 	
@@ -56,41 +69,43 @@ func _get_input():
 	elif velocity.x < 0:
 		$Sprite.flip_h = true
 		collision.position = Vector2(-6, -8)
+		
+	
+	############ funcoes sempre rodando
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	_set_animation()
+	
 
-
-func attack() -> void:
-	if Input.is_action_just_pressed("Ataque") and can_attack == true && is_on_floor():
-		can_attack = false
-
-func _input(event: InputEvent):
-	#"ui_select = tecla espaço"
+func _unhandled_input(event):
 	if event.is_action_pressed("ui_select") && is_on_floor():
-		velocity.y = jump_force / 2
+		velocity.y = jump_force /2
+		return
+	elif event.is_action_pressed("Ataque") && is_on_floor():
+		can_attack = false
+		_set_animation()
+		return
 
 func _set_animation():
 	#pode se mexer?
-	if can_move == false:
+	set_physics_process(true)
+	if health > 0:
+		if velocity.x == 0:
+			anim = "idle"
+		elif velocity.x != 0:
+			anim = "run"
+
+	if can_attack == false:
+		anim = "attack"
 		set_physics_process(false)
-	else:
-		set_physics_process(true)
-		if health > 0:
-			if velocity.x == 0:
-				anim = "idle"
-			elif velocity.x != 0:
-				anim = "run"
 
-		if can_attack == false:
-			anim = "attack"
-			set_physics_process(false)
+	elif health < 1:
+		anim = "dead"
+		dead = true
+		$Collision.disabled = true
+		set_physics_process(false)
 
-		elif health < 1:
-			anim = "dead"
-			dead = true
-			$Collision.disabled = true
-			set_physics_process(false)
-
-		if $anim.assigned_animation != anim:
-			$anim.play(anim)
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
 
 func _on_anim_animation_finished(animation):
 	if animation == "attack":
