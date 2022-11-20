ccce0a8f27e615aa6a5bcaf11be5c41bc812cad5
joao <kandrocmatunein@gmail.com>
2022-11-19 23:32:13 -0300

    F button

--- capuz_verde/scripts/saveArea.gd
+++ capuz_verde/scripts/saveArea.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
-var interacao = false
+export (NodePath) var _animation_keyboard
+onready var animation_keyboard: AnimationPlayer = get_node(_animation_keyboard)
 export var map_local : String
+var interacao = false
+
+func _ready():
+	animation_keyboard.play("Idle")
 
 func _process(delta):
-	if interacao == true and Input.is_action_pressed("ui_up"):		
+	if interacao == true and Input.is_action_pressed("f_button"):		
 		#pega a posicao atual da planta e passa pro jogador
 		Global.player_position_x = position.x
 		Global.player_position_y = position.y - 10
@@ -18,8 +23,10 @@ func _process(delta):
 
 func _on_saveArea_body_entered(body):
 	interacao = true
+	animation_keyboard.play("Up")
 func _on_saveArea_body_exited(body):
 	interacao = false
+	animation_keyboard.play("Idle")
 
 
 func _on_TimerParticles_timeout():
